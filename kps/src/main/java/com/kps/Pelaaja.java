7cafe1c0e092d88c6f80c3d30c2dd7ce40175972
Mikko <104062512+mikkoolh@users.noreply.github.com>
2024-04-30 08:20:17 +0300

    Add files via upload

--- kps/src/main/java/com/kps/Pelaaja.java
+++ kps/src/main/java/com/kps/Pelaaja.java
@@ -1,33 +1,75 @@
-
 package com.kps;
 
 /**
+ * Luokka Pelaaja edustaa pelaajia Kivi-paperi-sakset-pelissä.
  *
  * @author Ira Dook
  */
 public class Pelaaja {
 
+    /**
+     * Pelaajan voittojen lukumäärä.
+     */
     private int voitot;
+
+    /**
+     * Pelaajan valitsema vaihtoehto (kivi, paperi tai sakset).
+     */
     private String valinta;
+
+    /**
+     * Taulukko, joka sisältää pelin vaihtoehdot.
+     */
     private String[] vaihtoehdot = {"kivi", "paperi", "sakset"};
 
     /**
-     * Arvo kivi, paperi tai sakset ja tallenna se 
+     * Arpoo satunnaisesti uuden valinnan ja asettaa sen pelaajan valinnaksi.
      */
-
     public void arvoValinta() {
         valinta = vaihtoehdot[(int) (Math.random() * 3)];
     }
 
+    /**
+     * Palauttaa pelaajan valitseman vaihtoehdon.
+     *
+     * @return Pelaajan valitsema vaihtoehto (kivi, paperi tai sakset).
+     */
     public String getValinta() {
         return valinta;
     }
 
+    /**
+     * Lisää pelaajan voittojen lukumäärää yhdellä.
+     */
     public void lisaaVoitto() {
         voitot++;
     }
 
+    /**
+     * Palauttaa pelaajan voittojen yhteenlasketun lukumäärän.
+     *
+     * @return Pelaajan voittojen lukumäärä.
+     */
     public int getVoitot() {
         return (voitot);
     }
+
+    /**
+     * Palauttaa pelin vaihtoehdot taulukkona.
+     *
+     * @return Taulukko, joka sisältää pelin vaihtoehdot.
+     */
+    public String[] getVaihtoehdot() {
+        return vaihtoehdot;
+    }
+
+    /**
+     * Asettaa valinnan jupiter testiä varten. Ei käytetä pelissä.
+     *
+     */
+
+    public void setValinta(String str) {
+        valinta = str;
+    }
 }
+
--- kps/src/main/java/com/kps/Peli.java
+++ kps/src/main/java/com/kps/Peli.java
@@ -1,65 +1,137 @@
 package com.kps;
 
 /**
+ * Luokka Peli edustaa Kivi-paperi-sakset-peliä.
  *
  * @author Ira Dook
  */
 public class Peli {
-        private Pelaaja p1 = new Pelaaja();
-        private Pelaaja p2 = new Pelaaja();
-        private boolean kaynnissa = true;
-        private int pelatutPelit = 0;
-        private int tasapelit = 0;
-       
-        public void pelaa() {
-            while(kaynnissa) {
-                tulostaTilanne();
 
-                pelaaKierros();
+    /**
+     * Pelaaja 1.
+     */
+    private Pelaaja p1 = new Pelaaja();
 
-                vertaaValinnat();             
+    /**
+     * Pelaaja 2.
+     */
+    private Pelaaja p2 = new Pelaaja();
 
-                tarkistaLoppuminen();
-            }
+    /**
+     * Määrittää, onko peli käynnissä.
+     */
+    private boolean kaynnissa = true;
+
+    /**
+     * Pelattujen kierrosten lukumäärä.
+     */
+    private int pelatutPelit = 0;
+
+    /**
+     * Tasapelien lukumäärä.
+     */
+    private int tasapelit = 0;
+
+    /**
+     * Käynnistää Kivi-paperi-sakset-pelin. Sisältää ohjelman rungon/toiminnot.
+     */
+    public void pelaa() {
+        while(kaynnissa) {
+            tulostaTilanne();
+
+            pelaaKierros();
+
+            vertaaValinnat();             
+
+            tarkistaLoppuminen();
+        }
+    }
+
+    /**
+     * Tulostaa pelin tilannetiedot.
+     */
+    private void tulostaTilanne() {
+        System.out.printf("\nErä: %d ================\n", pelatutPelit + 1);
+        System.out.printf("Tasapelien lukumäärä: %d\n", tasapelit);
+    }
+
+    /**
+     * Vertaa pelaajien valintoja ja määrittää voittajan.
+     */
+    void vertaaValinnat() {
+
+        if (p1.getValinta().equals(p2.getValinta())) {
+            tasapelit++;
+            System.out.println("\n\t\t Tasapeli \n");
+            return;
         }
 
-        private void tulostaTilanne() {
-            System.out.printf("\nErä: %d ================\n", pelatutPelit + 1);
-            System.out.printf("Tasapelien lukumäärä: %d\n", tasapelit);
+        if (p1.getValinta().equals("kivi") && p2.getValinta().equals("sakset")
+                || p1.getValinta().equals("paperi") && p2.getValinta().equals("kivi")
+                || p1.getValinta().equals("sakset") && p2.getValinta().equals("paperi")) {
+            p1.lisaaVoitto();
+        } else {
+            p2.lisaaVoitto();
         }
+    }
 
-        private void vertaaValinnat() {
-    
-            if (p1.getValinta().equals(p2.getValinta())) {
-                tasapelit++;
-                System.out.println("\n\t\t Tasapeli \n");
-                return;
-            }
-    
-            if (p1.getValinta().equals("kivi") && p2.getValinta().equals("sakset")
-                    || p1.getValinta().equals("paperi") && p2.getValinta().equals("kivi")
-                    || p1.getValinta().equals("sakset") && p2.getValinta().equals("paperi")) {
-                p1.lisaaVoitto();
+    /**
+     * Pelaa yksi kierros Kivi-paperi-sakset-peliä ja kasvattaa pelatut pelit laskuria.
+     */
+    private void pelaaKierros() {
+        pelatutPelit++;
+
+        p1.arvoValinta();
+        p2.arvoValinta();
+        System.out.println();
+
+        System.out.println("Pelaaja 1: " + p1.getValinta() + "\n\t Pelaaja 1:llä koossa " + p1.getVoitot() + " voittoa.");
+        System.out.println("Pelaaja 2: " + p2.getValinta() + "\n\t Pelaaja 2:lla koossa " + p2.getVoitot() + " voittoa.");
+    }
+
+    /**
+     * Tarkistaa, onko peli ohi ja tulostaa pelin tuloksen.
+     */
+    private void tarkistaLoppuminen() {
+        if ((p1.getVoitot() >= 3) || (p2.getVoitot() >= 3)) {
+            kaynnissa = false;
+            System.out.println("\nKOLME VOITTOA - PELI PÄÄTTYY");
+
+            if (p1.getVoitot() > p2.getVoitot()) {
+                System.out.println("Pelaaja 1 voittaa!");
+            } else if (p1.getVoitot() < p2.getVoitot()) {
+                System.out.println("Pelaaja 2 voittaa!");
             } else {
-                p2.lisaaVoitto();
+                System.out.println("Tasapeli!");
             }
         }
+    }
 
-        private void pelaaKierros() {
-            pelatutPelit++;
+    /**
+     * Palauttaa listan pelaajista. Käytetään testaamiseen.
+     * @return pelaajalista
+     */
 
-            p1.arvoValinta();
-            p2.arvoValinta();
-            System.out.println();
-
-            System.out.println("Pelaaja 1: " + p1.getValinta() + "\n\t Pelaaja 1:llä koossa " + p1.getVoitot() + " voittoa.");
-            System.out.println("Pelaaja 2: " + p2.getValinta() + "\n\t Pelaaja 2:lla koossa " + p2.getVoitot() + " voittoa.");
+    public Pelaaja[] getPlayers() {
+        Pelaaja[] plrs = {p1, p2};
+        return plrs;
         }
 
-        private void tarkistaLoppuminen() {
-            if ((p1.getVoitot() >= 3) || (p2.getVoitot() >= 3)) {
-                kaynnissa = false;
-                System.out.println("\nKOLME VOITTOA - PELI PÄÄTTYY");
-            }
-        }
+    /**
+     * Palauttaa tasapelien määrän. Käytetään testaamiseen.
+     * @return tasapelien lukumäärä.
+     */
+
+    public int getTasapelit() {
+        return tasapelit;
+    }
+
+    /**
+     * Palauttaa käynnissä olemisen totuusarvon.
+     * @return käynnissä bloolean.
+     */
+
+    public boolean getKaynnissa(){
+        return kaynnissa;
+    }
 }
